b169fd12795b1166da97db2c2bf576d2d8700567
Ali-Guliyev <ali.guliyevoo7@gmail.com>
2022-07-21 17:47:20 +0300

    app finished

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div v-if="quote.content">
+    <h3>"{{ quote.content }}"</h3>
+    <p>-{{ quote.author }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="generateQuote()">Generate new Quote</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+
+export default {
+  setup() {
+    const quote = ref({});
+
+    const generateQuote = async () => {
+      await axios.get('https://api.quotable.io/random').then((res) => {
+        quote.value = res.data;
+      });
+    };
+
+    return { generateQuote, quote };
+  },
+};
+</script>
